648951f92eedfb009a596e67f362711a7f8ec335
fdrgrhppfhfq <87792370+MKChou@users.noreply.github.com>
2025-05-31 12:32:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.getElementById('testBtn').addEventListener('click', function() {
-  document.getElementById('msg').textContent = '你剛剛點了按鈕！';
+// 表單送出事件
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('formMsg').textContent = "感謝您的來信，我們會盡快回覆！";
+  this.reset();
 });
