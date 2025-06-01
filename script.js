0267913e844cfbc2f7ba7bc74e84521021c2fef0
fdrgrhppfhfq <87792370+MKChou@users.noreply.github.com>
2025-06-01 11:33:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// 表單送出事件
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('formMsg').textContent = "感謝您的來信，我們會盡快回覆！";
-  this.reset();
-});
+
